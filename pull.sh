73925a59dcbec2842ad2e92be1a8cfcf66ba1a63
guodp <guodp9u0@gmail.com>
2017-02-22 15:40:23 +0800

    Modify Script

--- pull.sh
+++ pull.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo $(date +%y_%m_%d_%H_%I_%T) >> HexoBlogPull.log     
-echo "Begin Pull" >> HexoBlogPull.log
+echo "--------------------Begin--------------------" >> HexoBlogPull.log
 git pull >> HexoBlogPull.log
+echo "--------------------End--------------------" >> HexoBlogPull.log
 echo $(date +%y_%m_%d_%H_%I_%T) >> HexoBlogPull.log
